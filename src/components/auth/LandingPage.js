edefd2801f47b128a4d856ffb067b0529f0d829d
vidyasagar-at-268766070666 <vidyasagar.muttak@empower-solutions.co.in>
2020-09-15 12:26:10 +0530

    Landing page changed

--- src/components/auth/LandingPage.js
+++ src/components/auth/LandingPage.js
@@ -1,125 +1,193 @@
 import React, { Component } from "react";
-import { Auth } from 'aws-amplify';
-import TextField from '@material-ui/core/TextField';
-import { Link } from "react-router-dom";
+import logo from "../../assets/images/dropzne.png";
+import TextField from "@material-ui/core/TextField";
+import { Auth } from "aws-amplify";
+import { v4 as uuidv4 } from "uuid";
+import Footer from "../Footer";
+import { withRouter, Link } from "react-router-dom";
+document.body.classList.add("bodyStyle");
 
-class Landingpage extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
-    const isUserLoggedIn = this.checkIsUserLoggedIn();
-    console.log("Is Logged In user",isUserLoggedIn);
-    if (isUserLoggedIn) {
-      this.redirectTo();
+
+    this.state = {
+      username: "",
+      password: "",
+      errorMessage: null,
+      loading:false
+    };
+  }
+
+  loginClick = async (event) => {
+    event.preventDefault();
+    const { centralStore } = this.props;
+ 
+    centralStore.showLoader()
+    console.log("Error Message===>", this.state.errorMessage);
+    if (!this.state.errorMessage) {
+      await Auth.signIn(this.state.username, this.state.password)
+        .then((userResponse) => {
+          this.props.auth.setAuthStatus(true);
+          this.props.auth.setUser(userResponse);
+          centralStore.hideLoader()
+
+          this.props.history.push("/home");
+        })
+
+        .catch((error) => {
+         
+          console.log(error.message);
+          if(this.state.username && this.state.password===""){
+            this.setState({errorMessage:"Password Cannot be Empty"})
+            return
+          }else{
+          this.setState({ errorMessage: error.message });
+          }
+          centralStore.hideLoader()
+        });
     }
-    this.state = {};
-
-  }
-
-  redirectTo = () => {
-    this.props.history.push("/Message");
-  }
+    console.log(this.props.auth);
+  };
 
   checkIsUserLoggedIn = () => {
-    return this.props && this.props.auth && this.props.auth.isAuthenticated && this.props.auth.user;
-  }
-  
+    return (
+      this.props &&
+      this.props.auth &&
+      this.props.auth.isAuthenticated &&
+      this.props.auth.user
+    );
+  };
 
-  addBodyClass = () => {
-    document.body.classList.add("bodyStyle");
-  }
+  onInputChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+      errorMessage: null,
+    });
+  };
 
-  handleGoogleSignUp = async event => {
-    try {
-      this.setState({ isAuthenticating: true });
-      event.preventDefault();
-      await Auth.federatedSignIn({provider: 'Google'})
-      this.redirectTo();
-
-    } catch (error) {
-      this.setState({
-        errors: {
-          errMessage: error.message
-        }
-      });
+  handleUserAuthorize = async (element) => {
+    let cognitoUser = this.props.auth;
+    if (this.props.auth.user) {
+      cognitoUser = this.props.auth.user;
     }
-  }
-
-  handleFacebookSignUp = async event => {
-    try {
-      this.setState({ isAuthenticating: true });
-      event.preventDefault();
-      await Auth.federatedSignIn({provider: 'Facebook'})
-
-      this.props.history.push("/Message");
-
-    } catch (error) {
-      this.setState({
-        errors: {
-          errMessage: error.message
-        }
-      });
+    if (this.props.auth.user && this.props.auth.user.attributes) {
+      cognitoUser = this.props.auth.user.attributes;
     }
-  }
+    if (
+      this.props.auth.user &&
+      this.props.auth.user.signInUserSession.idToken.payload
+    ) {
+      cognitoUser = this.props.auth.user.signInUserSession.idToken.payload;
+    }
+    let userid_exist = false;
+    if (cognitoUser["custom:userid"]) {
+      userid_exist = cognitoUser["custom:userid"];
+    }
+
+    if (userid_exist) {
+      console.log("exitLogin is True");
+      this.props.history.push("/home");
+    } else {
+      let userid = uuidv4();
+      console.log("userid", userid);
+      let user = this.props.auth.user;
+      const attributes = {
+        "custom:userid": userid,
+      };
+
+      await Auth.updateUserAttributes(user, attributes);
+      const users = await Auth.currentAuthenticatedUser({ bypassCache: true });
+      this.props.auth.setUser(users);
+
+      {
+        let cognitoUser = this.props.auth;
+        console.log("cognitoUser", cognitoUser);
+        if (this.props.auth.user) {
+          cognitoUser = this.props.auth.user;
+        } else if (this.props.auth.user && this.props.auth.user.attributes) {
+          cognitoUser = this.props.auth.user.attributes;
+        }
+        if (
+          this.props.auth.user &&
+          this.props.auth.user.signInUserSession.idToken.payload
+        ) {
+          cognitoUser = this.props.auth.user.signInUserSession.idToken.payload;
+        }
+        const parameters = {
+          "custom:userid": userid,
+        };
+        console.log("parameters", parameters);
+      }
+    }
+  };
+
+  componentDidMount = () => {
+    if (this.checkIsUserLoggedIn()) {
+      this.handleUserAuthorize();
+    } else {
+      console.log("False");
+    }
+  };
 
   render() {
-    this.addBodyClass();
     return (
-      <div className="wrapper">
-      <div className="landing d-flex flex-column flex-grow-1">
-        <div className="flex-grow-1 d-flex justify-content-center align-items-sm-center">
-          <div className="white-radius-box box">
-            <a href="/" className="login-logo">
-              <img src={landingLogo} alt="" />
-            </a>
-            <h5 className="box-heading text-center">HEYWATCH.ME</h5>
-            <div className="text-center tell-to-fans">
-              <h1>Tell your fans</h1>
-              <p className="sub-heading">Get paid for your influence.</p>
+      <>
+      
+      <div className="login-wrapper">
+         
+        <div className="login-box-outer">
+          <a href="/" className="login-logo">
+            <img src={logo} alt="" className="img-fluid" />
+          </a>
+          <div className="login-box white-radius-box">
+            <h5 className="box-heading text-center">Log In</h5>
+            {
+            this.state.errorMessage?<div className="alert alert-danger">{this.state.errorMessage} </div>:null
+  }
+            <div className="login-fields">
+              <div className="form-field">
+                <TextField
+                  label="Email"
+                  fullWidth={true}
+                  type="username"
+                  id="username"
+                  value={this.state.email}
+                  onChange={this.onInputChange}
+                />
+              </div>
+              <div className="form-field">
+                <TextField
+                  label="Password"
+                  fullWidth={true}
+                  type="password"
+                  id="password"
+                  value={this.state.password}
+                  onChange={this.onInputChange}
+                />{" "}
+              </div>
+              <div className="form-field text-center">
+             
+                <Link to="/ForgotPassword" className="link-forgot-password">
+                  Forgot Password?
+                </Link>
+              </div>
             </div>
-            <div className="d-flex align-items-center justify-content-between social-login">
-              <button
-                className="btn btn-outline btn-facebook"
-                variant="outline-dark" onClick={() => Auth.federatedSignIn({provider: 'Facebook'})} onSubmit={this.handleSubmit}
-              >
-                <i className="fab fa-facebook-f fa-lg mr-2"></i>Continue
-              </button>
-
-              <button
-                className="btn btn-outline btn-google"
-                variant="outline-dark" onClick={() => Auth.federatedSignIn({provider: 'Google'})} onSubmit={this.handleSubmit}
-              >
-                <i className="fab fa-google fa-lg mr-2"></i>Continue
-              </button>
-            </div>
-
             <div>
-              <a href="Signuppage" className="btn btn-outline w-100">
-                SIGN UP
-              </a>
-            </div>
-            <p className="already-member text-center mt-4 mb-0">
-              Already a member?
-              <a href="Loginpage" className="ml-1">
+              <button className="btn btn-primary" onClick={this.loginClick}>
                 Log In
-              </a>
+              </button>
+            </div>
+            <p className="already-member">
+              Not a member?
+              <Link to="/Signup">Sign Up</Link>
             </p>
           </div>
         </div>
-
-        <footer className="login-footer text-center">
-          <ul>
-          <li><a href="#">About HeyWatch.Me</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">Help</a></li>
-          </ul>
-        </footer>
+        <Footer />
       </div>
-    </div>
-  
+      </>
     );
   }
 }
-
-export default Landingpage;
+export default withRouter(Login);
